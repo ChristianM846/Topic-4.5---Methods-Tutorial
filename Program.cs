8a651ed4dbb9a24296e21871713eb293cd3f943e
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-11 08:21:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_4._5___Methods_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static void Joke()
+        {
+
+        }
+
+
+    }
+}
