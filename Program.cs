4d59f8404a8549048118e151fa13108e45112e5c
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-11 09:18:00 -0400

    Cool

--- Program.cs
+++ Program.cs
@@ -1,24 +1,185 @@
-﻿namespace Topic_4._5___Methods_Tutorial
+﻿using System.Runtime.CompilerServices;
+
+namespace Topic_4._5___Methods_Tutorial
 {
+    // Christian Moyes
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press ENTER for a joke:");
-            Console.ReadLine();
-            Joke();
+            int bugs;
+            int choice;
+            bool done = false;
+
+            while (done == false)
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine();
+                Console.WriteLine("1 - Joke");
+                Console.WriteLine("2 - Ascii");
+
+                while (!Int32.TryParse(Console.ReadLine().Trim(), out choice) || choice != 1 && choice != 2)
+                {
+                    Console.WriteLine("That is not a valid choice, try again.");
+                }
+
+                if (choice == 1)
+                {
+                    Console.WriteLine("What kind of joke would you like?");
+                    Console.WriteLine();
+                    Console.WriteLine("1 - Computer joke");
+                    Console.WriteLine("2 - knock-knock joke");
+
+                    while (!Int32.TryParse(Console.ReadLine().Trim(), out choice) || choice != 1 && choice != 2)
+                    {
+                        Console.WriteLine("That is not a valid choice, try again");
+                    }
+
+                    if (choice == 1)
+                    {
+                        Console.WriteLine("How many bugs are in the code?");
+
+                        while (!Int32.TryParse(Console.ReadLine().Trim(), out bugs) || bugs < 0)
+                        {
+                            Console.WriteLine("That doesn't even make sense, try again.");
+                        }
+
+                        if (bugs == 0)
+                        {
+                            Console.WriteLine("Oh, okay... I can't really make a joke about that :(");
+                        }
+                        else
+                        {
+                            Joke(bugs);
+                        }
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.WriteLine("NOT READY YET");
+                    }
+
+
+
+                }
+
+
+
+
+
+            }
+
+
+            
         }
 
-        public static void Joke()
+        public static void Joke(int numBugs)
         {
-            Console.WriteLine("99 little bugs in the code");
-            Thread.Sleep(500);
-            Console.WriteLine("99 little bugs.");
-            Thread.Sleep(500);
-            Console.WriteLine("Fix a bug, run in again,");
-            Thread.Sleep(500);
-            Console.WriteLine("100 little bugs in the code");
+            Console.WriteLine($"{numBugs} little bugs in the code");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{numBugs} little bugs.");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Fix a bug, run in again,");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{numBugs + 1} little bugs in the code");
         }
+
+        public static void DrawSpider()
+        {
+            Console.WriteLine();
+            Console.WriteLine("                   ,,,,");
+            Console.WriteLine("             ,;) .';;;;',");
+            Console.WriteLine(" ;;,,_,-.-.,;;'_,|(\\;;;/),,_");
+            Console.WriteLine("  `';;/:|:);{ ;;;|| \\;/ /;;;\\__");
+            Console.WriteLine("      L;/-';/ \\;;\\',/;\\/;;;.') \\");
+            Console.WriteLine("      .:`''` - \\;;'.__/;;;/  . _'-._");
+            Console.WriteLine("    .'/   \\     \\;;;;;;/.'_7:.  '). \\_");
+            Console.WriteLine("  .''/     | '._ );}{;//.'    '-:  '.,\\");
+            Console.WriteLine(".'. /       \\  ( |;;;/_/         \\._./;\\   _,");
+            Console.WriteLine(" . /        |\\ ( /;;/_/             ';;;\\,;;_,");
+            Console.WriteLine(". /         )__(/;;/_/                (;;'''''");
+            Console.WriteLine(" /        _;:':;;;;:';-._             );");
+            Console.WriteLine("/        /   \\  `'`   --.'-._         \\/");
+            Console.WriteLine("       .'     '.  ,'         '-,");
+            Console.WriteLine("      /    /   r--,..__       '.\\");
+            Console.WriteLine("    .'    '  .'        '--._     ]");
+            Console.WriteLine("    (     :.(;>        _ .' '- ;/");
+            Console.WriteLine("    |      /:;(    ,_.';(   __.'");
+            Console.WriteLine("     '- -'\"|;:/    (;;;;-'--'");
+            Console.WriteLine("           |;/      ;;(");
+            Console.WriteLine("           ''      /;;|");
+            Console.WriteLine("                   \\;;|");
+            Console.WriteLine("                    \\/");
+            Console.WriteLine();
+            Console.WriteLine("Get me more pictures of Spiderman!");
+            Console.WriteLine();
+        }
+
+        public static void DrawChar()
+        {
+            Console.WriteLine();
+            Console.WriteLine("              _.--\"\"`-..");
+            Console.WriteLine("            ,'          `.");
+            Console.WriteLine("           ,'          __  `.");
+            Console.WriteLine("           /|          \" __   \\");
+            Console.WriteLine("            , |           / |.   .");
+            Console.WriteLine("         |,'          !_.'|   |");
+            Console.WriteLine("         ,'             '   |   |");
+            Console.WriteLine("      /              |`--'|   |");
+            Console.WriteLine("       |                `---'   |");
+            Console.WriteLine("       .   ,                   |                       ,\".");
+            Console.WriteLine("      ._     '           _'  |                    , ' \\ `");
+            Console.WriteLine("  `.. `.`-...___,...---\"\"    |       __,.        ,`\"   L,|");
+            Console.WriteLine("    |, `- .`._        _,-,.'   .  __.-'-. /        .   ,    \\");
+            Console.WriteLine("-:..     `. `-..--_.,.<       `\"      / `.        `-/ |   .");
+            Console.WriteLine("   `,         \"\"\"\"'     `.              ,'         |   |  ',,");
+            Console.WriteLine("    `.      '            '            /          '    |'. |/");
+            Console.WriteLine("      `.   |              \\       _,-'           |       ''");
+            Console.WriteLine("        `._'               \\   '\"\\                .      |");
+            Console.WriteLine("            |                '     \\                `._  ,'");
+            Console.WriteLine("           |                 '     \\                 .'|");
+            Console.WriteLine("           |                 .      \\                | |");
+            Console.WriteLine("            |                 |       L              ,' |");
+            Console.WriteLine("            `                 |       |             /   '");
+            Console.WriteLine("             \\                |       |           ,'   /");
+            Console.WriteLine("           ,' \\               |  _.._ ,-..___,..-'    ,'");
+            Console.WriteLine("          /     .             .      `!             ,/'");
+            Console.WriteLine("         /       `.          /        .           .'/");
+            Console.WriteLine("        .          `.       /         |        _.'.'");
+            Console.WriteLine("         `.          7`'---'          |------\"'_.'");
+            Console.WriteLine("        _,.`,_     _'                ,''-----\"'");
+            Console.WriteLine("     _,-_    '       `.     .'      ,\\");
+            Console.WriteLine("    -\" /`.         _,'     | _  _  _.|");
+            Console.WriteLine("     \"\"--'---\"\"\"\"\"'        `' '! |! /");
+            Console.WriteLine("                             `\" \" -'");
+            Console.WriteLine();
+            Console.WriteLine("Charmander used ember!");
+            Console.WriteLine();
+        }
+
+        public static void TrollFace()
+        {
+            Console.WriteLine("⠀⠀⠀⠀⠀⠀⢀⣤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⢤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀");
+            Console.WriteLine("⠀⠀⠀⠀⢀⡼⠋⠀⣀⠄⡂⠍⣀⣒⣒⠂⠀⠬⠤⠤⠬⠍⠉⠝⠲⣄⡀⠀⠀");
+            Console.WriteLine("⠀⠀⠀⢀⡾⠁⠀⠊⢔⠕⠈⣀⣀⡀⠈⠆⠀⠀⠀⡍⠁⠀⠁⢂⠀⠈⣷⠀⠀");
+            Console.WriteLine("⠀⠀⣠⣾⠥⠀⠀⣠⢠⣞⣿⣿⣿⣉⠳⣄⠀⠀⣀⣤⣶⣶⣶⡄⠀⠀⣘⢦⡀");
+            Console.WriteLine("⢀⡞⡍⣠⠞⢋⡛⠶⠤⣤⠴⠚⠀⠈⠙⠁⠀⠀⢹⡏⠁⠀⣀⣠⠤⢤⡕⠱⣷");
+            Console.WriteLine("⠘⡇⠇⣯⠤⢾⡙⠲⢤⣀⡀⠤⠀⢲⡖⣂⣀⠀⠀⢙⣶⣄⠈⠉⣸⡄⠠⣠⡿");
+            Console.WriteLine("⠀⠹⣜⡪⠀⠈⢷⣦⣬⣏⠉⠛⠲⣮⣧⣁⣀⣀⠶⠞⢁⣀⣨⢶⢿⣧⠉⡼⠁");
+            Console.WriteLine("⠀⠀⠈⢷⡀⠀⠀⠳⣌⡟⠻⠷⣶⣧⣀⣀⣹⣉⣉⣿⣉⣉⣇⣼⣾⣿⠀⡇⠀");
+            Console.WriteLine("⠀⠀⠀⠈⢳⡄⠀⠀⠘⠳⣄⡀⡼⠈⠉⠛⡿⠿⠿⡿⠿⣿⢿⣿⣿⡇⠀⡇⠀");
+            Console.WriteLine("⠀⠀⠀⠀⠀⠙⢦⣕⠠⣒⠌⡙⠓⠶⠤⣤⣧⣀⣸⣇⣴⣧⠾⠾⠋⠀⠀⡇⠀");
+            Console.WriteLine("⠀⠀⠀⠀⠀⠀⠀⠈⠙⠶⣭⣒⠩⠖⢠⣤⠄⠀⠀⠀⠀⠀⠠⠔⠁⡰⠀⣧⠀");
+            Console.WriteLine("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⢤⣀⣀⠉⠉⠀⠀⠀⠀⠀⠁⠀⣠⠏⠀");
+            Console.WriteLine("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠒⠲⠶⠤⠴⠒⠚⠁⠀⠀");
+            Console.WriteLine();
+            Console.WriteLine("You've");
+            Thread.Sleep(1000);
+            Console.WriteLine("Been");
+            Thread.Sleep(1000);
+            Console.WriteLine("TROLLED!");
+            Console.WriteLine();
+        }
+
 
 
     }
